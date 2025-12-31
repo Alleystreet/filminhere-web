1e3414c486ea3d7d1aa1c7d25aee18c4baf5491a
Sean Whitaker <sean.t.whitaker@gmail.com>
2025-12-31 03:15:05 -0500

    Phase 2 polish: hero CTA readability + layout tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@ import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
 import AudienceSplit from "./components/AudienceSplit";
 import TrustSafety from "./components/TrustSafety";
+import EarlyAccess from "./components/EarlyAccess";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <HowItWorks />
-      <AudienceSplit />
-      <TrustSafety />
-    </>
+  <Hero />
+  <HowItWorks />
+  <AudienceSplit />
+  <TrustSafety />
+  <EarlyAccess />
+</>
   );
 }
 
