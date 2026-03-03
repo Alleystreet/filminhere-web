841353161d0ddb05e81055f2ffad59d37046b803
Sean Whitaker <sean.t.whitaker@gmail.com>
2026-03-02 22:28:04 -0500

    chore: disable x-powered-by header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  poweredByHeader: false,
 };
 
 export default nextConfig;
